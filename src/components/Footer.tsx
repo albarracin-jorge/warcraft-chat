3464f94c77359ca6347f4bfb93e2d7809d7c9900
Jorge Albarracin <albarracinj.dev@gmail.com>
2026-02-14 20:56:20 -0300

    [add] footer with links

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+export const Footer = () => {
+  return(
+    <footer className="text-center text-xs md:text-sm text-amber-100/60 py-8 md:py-12 fantasy flex flex-col gap-2">
+      <p>
+        This project was created as a playground to explore the Warcraftcn library from <a className="hover:underline" href="https://github.com/TheOrcDev">OrcDev</a> and experiment with the Anthropic API 🙂
+      </p>
+      <p>
+        Crafted with 💜 by <a className="hover:underline" href="https://github.com/albarracin-jorge">Coco</a>
+      </p>
+    </footer>
+  )
+}
